4c1b927f1e0b0a61143aba445bc2173266bb82ef
Carlos Zabala <czabala847@gmail.com>
2022-07-09 11:20:01 -0500

    Optimizando busquedas http con httpParamas y corrigiendo para trabajar con la version 2 de la api

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 import { Country } from '../interfaces/country.interface';
 
@@ -9,28 +9,43 @@ import { Country } from '../interfaces/country.interface';
   providedIn: 'root',
 })
 export class CountryService {
-  private apiUrl: string = 'https://restcountries.com/v3.1/';
+  // private apiUrl: string = 'https://restcountries.com/v3.1/';
   private apiUrlV2: string = 'https://restcountries.com/v2/';
 
   constructor(private http: HttpClient) {}
 
+  get httpParams() {
+    return new HttpParams().set(
+      'fields',
+      'flags,name,capital,population,alpha3Code'
+    );
+  }
+
   searchCountry(country: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/name/${country}`);
+    return this.http.get<Country[]>(`${this.apiUrlV2}/name/${country}`, {
+      params: this.httpParams,
+    });
   }
 
   searchByCapital(capital: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${capital}`);
+    return this.http.get<Country[]>(`${this.apiUrlV2}/capital/${capital}`, {
+      params: this.httpParams,
+    });
   }
 
   searchByCode(id: string): Observable<Country> {
-    return this.http
-      .get<Country[]>(`${this.apiUrl}/alpha/${id}`)
-      .pipe(map((dataCountry) => dataCountry[0]));
+    return this.http.get<Country>(`${this.apiUrlV2}/alpha/${id}`);
   }
 
   searchByRegional(codeRegional: string): Observable<Country[]> {
-    return this.http.get<Country[]>(
-      `${this.apiUrlV2}regionalbloc/${codeRegional}`
-    );
+    return this.http
+      .get<Country[]>(`${this.apiUrlV2}regionalbloc/${codeRegional}`, {
+        params: this.httpParams,
+      })
+      .pipe(
+        tap((response) => {
+          console.log(response);
+        })
+      );
   }
 }
